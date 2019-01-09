254858a9956751aa75b7efef0a71e82c8e29ff12
Tom <Thomas.Magin@Cognizant.com>
2019-01-09 15:27:30 +1100

    Working on Jest, Enzyme and Regression Testing

---
+++ src/components/QuestionList.spec.js
@@ -0,0 +1,5 @@
+describe(`The questions list`, ()=> {
+    it(`Should display list of items`, ()=>{
+expect(2+3).toEqual(5);
+});
+});
---
+++ src/components/__tests__/QuestionDetail.js
@@ -0,0 +1,6 @@
+describe(`The Question Component`, ()=> {
+it(`Should not regress`, ()=> {
+
+    //lack of assertion is a pass
+});
+});
